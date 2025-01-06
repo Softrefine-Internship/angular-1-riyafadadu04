886d71f4bbb73058a7d6077175d79ec5557533f8
riyafadadu04 <riyafadadu2004@gmail.com>
2025-01-06 15:42:28 +0530

    Test-1 Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Profile } from './profile.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-1';
+  profiles: Profile[] = [
+    {
+      name: 'Riya',
+      email: 'riya@gmail.com',
+      phoneNo: 9876543210,
+      age: 21,
+      edit: false,
+    },
+    {
+      name: 'Priya',
+      email: 'priya@gmail.com',
+      phoneNo: 9876543210,
+      age: 22,
+      edit: false,
+    },
+    {
+      name: 'khushboo',
+      email: 'khushboo@gmail.com',
+      phoneNo: 9876543210,
+      age: 23,
+      edit: false,
+    },
+    {
+      name: 'shreya',
+      email: 'shreya@gmail.com',
+      phoneNo: 9876543210,
+      age: 23,
+      edit: false,
+    },
+  ];
 }
---
+++ src/app/profile.model.ts
@@ -0,0 +1,9 @@
+export class Profile {
+  public edit = false;
+  constructor(
+    public name: string,
+    public email: string,
+    public phoneNo: number,
+    public age: number
+  ) {}
+}
